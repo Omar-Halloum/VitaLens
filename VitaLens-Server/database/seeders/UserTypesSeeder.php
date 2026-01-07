831085cc95fa300a61d2d5ddb2bdba1f538cc675
Omar Halloum <ohalloum2003@gmail.com>
2026-01-07 20:21:59 +0200

    Created UserTypesSeeder

---
+++ VitaLens-Server/database/seeders/UserTypesSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\UserType;
+
+class UserTypesSeeder extends Seeder
+{
+    public function run(): void
+    {
+        UserType::create(['name' => 'Admin']);
+        UserType::create(['name' => 'User']);
+    }
+}
