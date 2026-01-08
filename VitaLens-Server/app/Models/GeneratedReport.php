03af19a2c6f96efd03afd7e72b9e64ff51e52f2b
Omar Halloum <ohalloum2003@gmail.com>
2026-01-08 22:19:52 +0200

    Created generated report model

--- VitaLens-Server/app/Models/GeneratedReport.php
+++ VitaLens-Server/app/Models/GeneratedReport.php
@@ -2,9 +2,28 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class GeneratedReport extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'clinic_id',
+        'file_name',
+        'is_uploaded_to_drive',
+    ];
+
+
+    public function patient()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function clinic()
+    {
+        return $this->belongsTo(Clinic::class);
+    }
 }
