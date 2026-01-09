9c7b4999b14d68c1bde59e3901eee671a9f59172
Omar Halloum <ohalloum2003@gmail.com>
2026-01-09 10:11:37 +0200

    Created medical document routes

--- VitaLens-Server/routes/api.php
+++ VitaLens-Server/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\MedicalDocumentController;
 
 
 // unauthenticated routes
@@ -13,4 +14,6 @@ Route::get('/error', [AuthController::class, "displayError"])->name("login");
 // authenticated routes
 Route::group(["prefix" => "v1", "middleware" => "auth:api"], function (){
 
+    Route::post('/upload-documents', [MedicalDocumentController::class, 'addDocument']);
+    Route::get('/get-documents', [MedicalDocumentController::class, 'getUserDocuments']);
 });
