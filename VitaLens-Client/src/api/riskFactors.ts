0ce5bc5ef6a07da8e594c3bdf3201bad4606a3ce
Omar Halloum <ohalloum2003@gmail.com>
2026-01-13 18:53:47 +0200

    Created riskFactors api

---
+++ VitaLens-Client/src/api/riskFactors.ts
@@ -0,0 +1,17 @@
+import { API_BASE_URL } from "../config/api";
+import { getAuthHeader } from "./auth-header.api";
+import type { RiskFactor } from "../types/riskFactors";
+
+export async function fetchRiskFactors(riskKey: string): Promise<RiskFactor[]> {
+  const res = await fetch(`${API_BASE_URL}/risk-factors/${riskKey}`, {
+    headers: getAuthHeader(),
+  });
+  
+  const json = await res.json();
+  
+  if (!res.ok) {
+    throw new Error(json.payload || "Failed to fetch risk factors");
+  }
+  
+  return json.payload;
+}
