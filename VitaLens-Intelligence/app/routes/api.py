bbcd10b06c27b4945f1025a32f79dea924ca8505
Omar Halloum <ohalloum2003@gmail.com>
2026-01-18 17:02:10 +0200

    Added route for risk prediction

--- VitaLens-Intelligence/app/routes/api.py
+++ VitaLens-Intelligence/app/routes/api.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from app.controllers import OCRController
 from app.controllers.RagController import router as rag_router
+from app.controllers.RiskController import router as risk_router
 
 router = APIRouter()
 
@@ -8,3 +9,6 @@ router.post("/ocr/extract")(OCRController.extract_text)
 
 # Mount the RAG router
 router.include_router(rag_router, prefix="/rag", tags=["RAG"])
+
+# Mount the Risk Prediction router
+router.include_router(risk_router, prefix="/predict", tags=["Risk Prediction"])
